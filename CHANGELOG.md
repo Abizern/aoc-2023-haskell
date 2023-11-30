9a5033885741d1b1f2e2aef3c27009d08b60d6d2
Abizer Nasir <abizern@mac.com>
2023-11-30 23:14:48 +0000

    Initial setup of project

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for aoc23
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ fetch_inputs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# Fetches all the inputs from 20151 and puts them in the relevant directory with the correct name
+for day in $(seq -w 1 25)
+do
+    aocd "2023" "$day" >| inputs/day"$day".txt
+    echo "Fetched input for $year $day"
+done
+
