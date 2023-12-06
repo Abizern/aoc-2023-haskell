ab736600163f3f3b922cd22123ab64eb1ea7a5d6
Abizer Nasir <abizern@mac.com>
2023-12-06 09:43:05 +0000

    partial Day05

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,15 +1,15 @@
--- usage: cabal run aoc23 05
--- description: 
+-- usage: cabal run aoc23 5
+-- description: Create functions on ranges.
 
 module Day05 where
 
-import AOCUtils.Input (makeTitle)
+import AOCUtils.Input (makeTitle, newlines)
 
 title :: String
-title = "--- Add day title here ---"
+title = "--- Day 5: If You Give A Seed A Fertilizer ---"
 
 solve :: String -> String
-solve s = mconcat [header, "Part 1: ", part1, "\nPart 2: ", part2, "\n"]
+solve s = mconcat ["\n", header, "Part 1: ", part1, "\nPart 2: ", part2, "\n"]
   where
     input = s
     header = makeTitle title
@@ -21,3 +21,12 @@ solve1 = id
 
 solve2 :: String -> String
 solve2 = id
+
+-- These functions are just here to make working with the REPL easier.
+
+rawExample :: IO String
+rawExample = readFile "examples/ex05.txt"
+
+parseInput s = raw
+  where
+    raw = lines s
