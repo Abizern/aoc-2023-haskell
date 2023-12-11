b819d906399480c1a34a3610b7d2083f8d963d9e
Abizer Nasir <abizern@mac.com>
2023-12-11 10:46:03 +0000

    Parked attempt day09 part2

--- src/Day08.hs
+++ src/Day08.hs
@@ -1,17 +1,18 @@
--- usage: cabal run aoc23 08
--- description:
+-- usage: cabal run aoc23 8
+-- description: Follow a route, and then find a cycle length.
 
 module Day08 where
 
 import AOCUtils.Input (makeTitle)
+import Data.List (isSuffixOf)
 import Data.Map (Map)
-import qualified Data.Map as Map (fromList, lookup)
+import qualified Data.Map.Strict as Map (fromList, keys, lookup)
 
 title :: String
-title = "--- Add day title here ---"
+title = "--- Day 8: Haunted Wasteland ---"
 
 solve :: String -> String
-solve s = mconcat [header, "Part 1: ", part1, "\nPart 2: ", part2, "\n"]
+solve s = mconcat ["\n", header, "Part 1: ", part1, "\nPart 2: ", part2, "\n"]
   where
     input = parseInput s
     header = makeTitle title
@@ -22,7 +23,7 @@ solve1 :: (String, Map String (String, String)) -> Int
 solve1 = runMapping
 
 solve2 :: (String, Map String (String, String)) -> Int
-solve2 _ = 2
+solve2 = runGhostMapping
 
 runMapping :: (String, Map String (String, String)) -> Int
 runMapping (instructions, mapping) = length $ run ["AAA"] (cycle instructions)
@@ -38,6 +39,31 @@ runMapping (instructions, mapping) = length $ run ["AAA"] (cycle instructions)
             Just x -> x
             Nothing -> error "Missing value in dictionary"
       _ -> accum
+
+runGhostMapping :: (String, Map String (String, String)) -> Int
+runGhostMapping (instructions, mapping) = runGhostMapping' 0 startPoints (cycle instructions)
+  where
+    routingFunction = mapper mapping
+    startPoints = startingPoints $ Map.keys mapping
+    isEndPoint :: [String] -> Bool
+    isEndPoint = foldr (\pos accum -> accum && ("Z" `isSuffixOf` pos)) True
+    runGhostMapping' :: Int -> [String] -> String -> Int
+    runGhostMapping' iterations ps is = case (iterations, ps, is) of
+      (iters, x, y : ys)
+        | isEndPoint x -> iters
+        | otherwise -> runGhostMapping' (iters + 1) newPoints ys
+        where
+          newPoints = map (routingFunction y) x
+      _ -> error "There should always be something in the accumulator"
+
+mapper :: Map String (String, String) -> Char -> String -> String
+mapper m direction s = case (Map.lookup s m, direction) of
+  (Just x, 'L') -> fst x
+  (Just x, 'R') -> snd x
+  _ -> error "No value for this key"
+
+startingPoints :: [String] -> [String]
+startingPoints = filter ("A" `isSuffixOf`)
 
 parseInput :: String -> (String, Map String (String, String))
 parseInput s = (instructions, mapping)
@@ -60,3 +86,6 @@ example1 = readFile "examples/ex08.txt"
 
 example2 :: IO String
 example2 = readFile "examples/ex08_2.txt"
+
+example3 :: IO String
+example3 = readFile "examples/ex08_3.txt"
