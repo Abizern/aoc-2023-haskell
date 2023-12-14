b7877b98a8ef9d9930637b0be81d5875544728c5
Abizer Nasir <abizern@mac.com>
2023-12-14 12:55:40 +0000

    Solve Day14 part one

--- fetch_inputs.sh
+++ fetch_inputs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Fetches all the inputs from 20151 and puts them in the relevant directory with the correct name
-for day in $(seq -w 1 13)
+for day in $(seq -w 1 14)
 do
     aocd "2023" "$day" >| inputs/day"$day".txt
     echo "Fetched input for $year $day"
--- src/Day14.hs
+++ src/Day14.hs
@@ -1,23 +1,46 @@
 -- usage: cabal run aoc23 14
--- description: 
+-- description: Rotate grids
 
 module Day14 where
 
 import AOCUtils.Input (makeTitle)
+import Data.List (transpose)
 
 title :: String
-title = "--- Add day title here ---"
+title = "--- Day 14: Parabolic Reflector Dish ---"
 
 solve :: String -> String
 solve s = mconcat [header, "Part 1: ", part1, "\nPart 2: ", part2, "\n"]
   where
-    input = s
+    input = lines s
     header = makeTitle title
     part1 = show $ solve1 input
     part2 = show $ solve2 input
 
-solve1 :: String -> String
-solve1 = id
+solve1 :: Grid -> Int
+solve1 = load . tilt
 
-solve2 :: String -> String
-solve2 = id
+solve2 :: Grid -> Int
+solve2 _ = 2
+
+type Grid = [String]
+
+-- | Move all the rocks that can move to the left in each row.
+roll :: String -> String
+roll = roll' (0 :: Int)
+  where
+    roll' n ('.' : gs) = roll' (n + 1) gs
+    roll' n ('O' : gs) = '0' : roll' n gs
+    roll' n ('#' : gs) = replicate n '.' ++ '#' : roll' 0 gs
+    roll' n _ = replicate n '.'
+
+tilt :: Grid -> Grid
+tilt g = transpose $ map roll $ transpose g
+
+load :: Grid -> Int
+load g = sum $ zipWith (*) [1 :: Int ..] (reverse rocks)
+  where
+    rocks = map (length . filter (== '0')) g
+
+example :: IO String
+example = readFile "examples/ex14.txt"
