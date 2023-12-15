cd7840fb55dba03da80b78fc440842931c1f4b7f
Abizer Nasir <abizern@mac.com>
2023-12-15 10:37:25 +0000

    Partial Day14 pt2

--- src/Day14.hs
+++ src/Day14.hs
@@ -4,13 +4,15 @@
 module Day14 where
 
 import AOCUtils.Input (makeTitle)
-import Data.List (transpose)
+import Data.List (elemIndex, transpose)
+import Data.Map (Map)
+import qualified Data.Map as Map (empty, lookup)
 
 title :: String
 title = "--- Day 14: Parabolic Reflector Dish ---"
 
 solve :: String -> String
-solve s = mconcat [header, "Part 1: ", part1, "\nPart 2: ", part2, "\n"]
+solve s = mconcat ["\n", header, "Part 1: ", part1, "\nPart 2: ", part2, "\n"]
   where
     input = lines s
     header = makeTitle title
@@ -18,7 +20,7 @@ solve s = mconcat [header, "Part 1: ", part1, "\nPart 2: ", part2, "\n"]
     part2 = show $ solve2 input
 
 solve1 :: Grid -> Int
-solve1 = load . tilt
+solve1 = load . north
 
 solve2 :: Grid -> Int
 solve2 _ = 2
@@ -34,13 +36,26 @@ roll = roll' (0 :: Int)
     roll' n ('#' : gs) = replicate n '.' ++ '#' : roll' 0 gs
     roll' n _ = replicate n '.'
 
-tilt :: Grid -> Grid
-tilt = transpose . map roll . transpose
+west :: Grid -> Grid
+west = map roll
+
+east :: Grid -> Grid
+east = map (reverse . roll . reverse)
+
+north :: Grid -> Grid
+north = transpose . west . transpose
+
+south :: Grid -> Grid
+south = transpose . east . transpose
 
 load :: Grid -> Int
 load g = sum $ zipWith (*) [1 :: Int ..] (reverse rocks)
   where
     rocks = map (length . filter (== '0')) g
+
+repeatIndices cs = repeatIndices' Map.empty Grid (0 :: Int) cs
+  where
+    repeatIndices' = undefined
 
 example :: IO String
 example = readFile "examples/ex14.txt"
