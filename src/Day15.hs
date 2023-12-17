715ae1514988809567709c8a84a7ded34f979963
Abizer Nasir <abizern@mac.com>
2023-12-17 16:36:02 +0000

    Solve Day15 part 2

--- src/Day15.hs
+++ src/Day15.hs
@@ -1,12 +1,14 @@
 -- usage: cabal run aoc23 15
--- description:
+-- description: Create a hash in part 1, use that as a key for a dictionary which is mutated.
 
 module Day15 where
 
 import AOCUtils.Input (makeTitle)
 import Data.Char (ord)
-import Data.List (foldl')
+import Data.List (elemIndex, foldl')
 import Data.List.Split (splitOn)
+import Data.Map (Map)
+import qualified Data.Map as Map (empty, findWithDefault, foldrWithKey, insert)
 
 title :: String
 title = "--- Day 15: Lens Library ---"
@@ -23,12 +25,60 @@ solve1 :: [String] -> Int
 solve1 = sum . map hasher
 
 solve2 :: [String] -> Int
-solve2 _ = 2
+solve2 l = focussingPower boxes
+  where
+    instructions = map parseInstruction l
+    boxes = foldl' processInstruction Map.empty instructions
 
 hasher :: String -> Int
 hasher = foldl' hasher' 0
   where
     hasher' accum ch = 17 * (accum + ord ch) `mod` 256
+
+data Lens = Lens String Int deriving (Show)
+
+-- | This makes it easier to check lenses in a box for removal and replacement
+instance Eq Lens where
+  Lens s1 _ == Lens s2 _ = s1 == s2
+
+type LBox = [Lens]
+
+type LBoxes = Map Int LBox
+
+data Instruction = Add Lens | Remove Lens deriving (Eq, Show)
+
+focussingPower :: LBoxes -> Int
+focussingPower = Map.foldrWithKey focussingPower' 0
+  where
+    boxPower bs = sum [n | (i, Lens _ f) <- zip [1 :: Int ..] bs, let n = i * f]
+    focussingPower' idx b accum = succ idx * boxPower b + accum
+
+parseInstruction :: String -> Instruction
+parseInstruction s = case splitOn "=" s of
+  [x] -> Remove $ Lens (init x) 0
+  [x, y] -> Add $ Lens x (read y)
+  _ -> error "Malformed input"
+
+processInstruction :: LBoxes -> Instruction -> LBoxes
+processInstruction boxes instruction =
+  let idx = box instruction
+      lbox = Map.findWithDefault [] idx boxes
+      process (Add l) = Map.insert idx (addLens l lbox) boxes
+      process (Remove l) = Map.insert idx (removeLens l lbox) boxes
+   in process instruction
+
+addLens :: Lens -> LBox -> LBox
+addLens l b = case elemIndex l b of
+  Just n -> take n b ++ [l] ++ drop (succ n) b
+  Nothing -> b ++ [l]
+
+removeLens :: Lens -> LBox -> LBox
+removeLens l = filter (/= l)
+
+box :: Instruction -> Int
+box i = case i of
+  Add (Lens s _) -> hasher s
+  Remove (Lens s _) -> hasher s
 
 parseInput :: String -> [String]
 parseInput = splitOn "," . takeWhile (/= '\n')
