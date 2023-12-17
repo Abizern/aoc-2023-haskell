0b6b4128f0a12a70a17969a958803c1915ae1c38
Abizer Nasir <abizern@mac.com>
2023-12-17 13:43:41 +0000

    Solve Day14 part 2

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,12 +1,12 @@
 -- usage: cabal run aoc23 14
--- description: Rotate grids
+-- description: Rotate grids finding and optimise evolving states with
 
 module Day14 where
 
 import AOCUtils.Input (makeTitle)
 import Data.List (elemIndex, transpose)
-import Data.Map (Map)
-import qualified Data.Map as Map (empty, lookup)
+import Data.Set (Set)
+import qualified Data.Set as Set (empty, insert, member)
 
 title :: String
 title = "--- Day 14: Parabolic Reflector Dish ---"
@@ -23,7 +23,12 @@ solve1 :: Grid -> Int
 solve1 = load . north
 
 solve2 :: Grid -> Int
-solve2 _ = 2
+solve2 g = load finalState
+  where
+    (start, grids) = platformStates g
+    cycleLength = length grids - start
+    cycleIndex = ((1000000000 - start) `rem` cycleLength) + start
+    finalState = grids !! cycleIndex
 
 type Grid = [String]
 
@@ -32,7 +37,7 @@ roll :: String -> String
 roll = roll' (0 :: Int)
   where
     roll' n ('.' : gs) = roll' (n + 1) gs
-    roll' n ('O' : gs) = '0' : roll' n gs
+    roll' n ('O' : gs) = 'O' : roll' n gs
     roll' n ('#' : gs) = replicate n '.' ++ '#' : roll' 0 gs
     roll' n _ = replicate n '.'
 
@@ -51,11 +56,27 @@ south = transpose . east . transpose
 load :: Grid -> Int
 load g = sum $ zipWith (*) [1 :: Int ..] (reverse rocks)
   where
-    rocks = map (length . filter (== '0')) g
+    rocks = map (length . filter (== 'O')) g
 
-repeatIndices cs = repeatIndices' Map.empty Grid (0 :: Int) cs
+cyclePlatform :: Grid -> Grid
+cyclePlatform = east . south . west . north
+
+platformStates :: Grid -> (Int, [Grid])
+platformStates = platformStates' Set.empty []
+
+platformStates' :: Set Grid -> [Grid] -> Grid -> (Int, [Grid])
+platformStates' set states grid = case (set, states, grid) of
+  (s, gs, g)
+    | Set.member g s -> processStates gs g
+    | otherwise -> platformStates' (Set.insert g s) (g : gs) (cyclePlatform g)
+
+processStates :: [Grid] -> Grid -> (Int, [Grid])
+processStates gs g = (start, grids)
   where
-    repeatIndices' = undefined
+    grids = reverse gs
+    start = case elemIndex g grids of
+      Just x -> x
+      _ -> error "The state really should exist"
 
 example :: IO String
 example = readFile "examples/ex14.txt"
