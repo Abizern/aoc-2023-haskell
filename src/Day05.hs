d3427a85c49398d33d0ede033f1f38ff86d49491
Abizer Nasir <abizern@mac.com>
2023-12-19 11:20:10 +0000

    Solve Day05 Part 1

--- src/Day05.hs
+++ src/Day05.hs
@@ -3,30 +3,61 @@
 
 module Day05 where
 
-import AOCUtils.Input (makeTitle, newlines)
-import Data.List.Split (splitOn)
+import AOCUtils.Input (makeTitle, newlines, spacedNumbers)
 import Data.Range
 
 title :: String
 title = "--- Day 5: If You Give A Seed A Fertilizer ---"
 
+type RangeMap = (Range Int, Int) -- The Range (+=*) and the offset to apply
+
+type Input = ([Int], [[RangeMap]])
+
 solve :: String -> String
 solve s = mconcat ["\n", header, "Part 1: ", part1, "\nPart 2: ", part2, "\n"]
   where
-    input = s
+    input = parseInput s
     header = makeTitle title
     part1 = show $ solve1 input
     part2 = show $ solve2 input
 
-solve1 :: String -> String
-solve1 = id
+solve1 :: Input -> Int
+solve1 (inputs, ranges) =
+  minimum
+    [ v
+      | i <- inputs,
+        let v = processMappings i ranges
+    ]
 
-solve2 :: String -> String
-solve2 = id
+solve2 :: Input -> Int
+solve2 _ = 2
 
-parseInput s = undefined
+processMappings :: Int -> [[RangeMap]] -> Int
+processMappings = foldl process
+  where
+    process i [] = i
+    process i (x : xs) = if inRange (fst x) i then i + snd x else process i xs
 
--- These functions are just here to make working with the REPL easier.
+-- Parsing Functions
+
+parseInput :: String -> Input
+parseInput = blocks . newlines
+  where
+    blocks (x : xs) = (parseSeeds x, map parseRangeMaps xs)
+    blocks _ = error "Malformed input"
+
+parseSeeds :: String -> [Int]
+parseSeeds = spacedNumbers . drop (length "seeds:")
+
+parseRangeMaps :: String -> [RangeMap]
+parseRangeMaps = map parseRangeMap . drop 1 . lines
+
+parseRangeMap :: String -> RangeMap
+parseRangeMap = parseRangeMap' . spacedNumbers
+  where
+    parseRangeMap' args = case args of
+      [d, s, l] -> (s +=* (s + l), d - s)
+      _ -> error "Malformed input"
 
 example :: IO String
 example = readFile "examples/ex05.txt"
