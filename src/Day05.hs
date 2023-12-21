113f41c2e4dd9145618297bd98561b4abae041b8
Abizer Nasir <abizern@mac.com>
2023-12-21 13:17:39 +0000

    Solve Day05 Part 2

--- src/Day05.hs
+++ src/Day05.hs
@@ -4,7 +4,7 @@
 module Day05 where
 
 import AOCUtils.Input (makeTitle, newlines, spacedNumbers)
-import Data.Range
+import Data.Range (Bound (Bound), Range (..), inRange, intersection, invert, mergeRanges, (+=*))
 
 title :: String
 title = "--- Day 5: If You Give A Seed A Fertilizer ---"
@@ -30,13 +30,54 @@ solve1 (inputs, ranges) =
     ]
 
 solve2 :: Input -> Int
-solve2 _ = 2
+solve2 (inputs, ranges) = minimum lowestValues
+  where
+    inputRanges = rangesFromSeeds inputs
+    processedRanges = foldl processRanges inputRanges ranges
+    lowestValues = map minOfRange processedRanges
 
+-- Given a location and a sequence of intermediate mappings, get the final location.
 processMappings :: Int -> [[RangeMap]] -> Int
 processMappings = foldl process
   where
     process i [] = i
     process i (x : xs) = if inRange (fst x) i then i + snd x else process i xs
+
+-- A folding function
+processRanges :: [Range Int] -> [RangeMap] -> [Range Int]
+processRanges = processRanges' []
+  where
+    processRanges' :: [Range Int] -> [Range Int] -> [RangeMap] -> [Range Int]
+    processRanges' acc ranges maps = case (acc, ranges, maps) of
+      (accum, rs, []) -> mergeRanges $ accum ++ rs
+      (accum, rs, x : xs) -> processRanges' (accum ++ newRanges) remainders xs
+        where
+          (rng, d) = x
+          int = intersection rs [rng]
+          inv = invert int
+          newRanges = map (offsetRange d) int
+          remainders = intersection inv rs
+
+minOfRange :: Range Int -> Int
+minOfRange (SingletonRange x) = x
+minOfRange (SpanRange (Bound val _) _) = val
+minOfRange _ = error "Should not have this type of range"
+
+-- Chunks input set list into ranges.
+rangesFromSeeds :: [Int] -> [Range Int]
+rangesFromSeeds [] = []
+rangesFromSeeds [_] = []
+rangesFromSeeds (x : y : xs) = x +=* (x + y) : rangesFromSeeds xs
+
+offsetRange :: Int -> Range Int -> Range Int
+offsetRange offset (SingletonRange x) = SingletonRange (x + offset)
+offsetRange offset (SpanRange x y) = SpanRange (offsetBound x offset) (offsetBound y offset)
+offsetRange offset (LowerBoundRange x) = LowerBoundRange (offsetBound x offset)
+offsetRange offset (UpperBoundRange x) = UpperBoundRange (offsetBound x offset)
+offsetRange _ InfiniteRange = InfiniteRange
+
+offsetBound :: Bound Int -> Int -> Bound Int
+offsetBound (Bound val t) offset = Bound (val + offset) t
 
 -- Parsing Functions
 
